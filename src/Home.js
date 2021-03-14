ecd3efa2b05057ad91ab11220c801a823ff9b8f2
VaidaEG <grausliene.vaida@gmail.com>
2021-03-14 16:06:42 +0200

    React using components and click events

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+const Home = () => {
+    const handleClick = (e) => {
+        console.log('Hello, ninjas!', e);
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
+
+
+    }
+    return ( 
+        <div className="home">
+            <h2>Homepage</h2>
+            <button onClick={ handleClick }>Click me</button>
+            <button onClick={ (e) => handleClickAgain('Mario', e)}>Click me again</button>
+        </div>
+    );
+}
+export default Home;
